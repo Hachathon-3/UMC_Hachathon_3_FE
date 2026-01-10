fba211b36575ead611916c1600f6f13148c9f773
moni <muwingky@gmail.com>
2026-01-11 01:56:28 +0900

    HomePage 구현

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,20 +1,20 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuthStore } from '../store/authStore';
 
 interface ProtectedRouteProps {
-    children: React.ReactElement;
+    children: React.ReactNode;
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-    // const { isAuthenticated } = useAuth();
+    // const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
     const isAuthenticated = true;
+    const location = useLocation();
 
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace />;
+        return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
-    return children;
+    return <>{children}</>;
 };
 
 export default ProtectedRoute;
-
